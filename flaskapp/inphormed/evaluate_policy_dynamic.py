e62304272f95d9427926a34e98903d8d187ccad5
kabuhakmeh3 <kabuhakmeh@gmail.com>
2019-09-26 20:51:01 -0700

    prioritized policy lists

--- flaskapp/inphormed/evaluate_policy_dynamic.py
+++ flaskapp/inphormed/evaluate_policy_dynamic.py
@@ -2,23 +2,25 @@
 
 import warnings
 warnings.filterwarnings("ignore")
+#import os, re, pickle, urllib
 import os, pickle, urllib
 #import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 from keras.models import load_model
+from keras import backend as K
 
 
 def standardize_text(df, text_field):
     '''Clean-up text column to prepare for tokenization
-    
+
     Removes unwanted characters &
     Replaces them with spaces or blanks
     --
     Input
     + pandas dataframe
     + name of text column
-    
+
     Returns
     + pandas dataframe with cleaned column
     '''
@@ -61,42 +63,63 @@ def get_sentences_from_html(html):
 
     return sentences
 
+def get_policy_action(policy):
+    '''
+    Parse Policies returned by NN classifier
+    Return:
+    + Policy name
+    + Action
+    '''
+    perf = '_PERFORMED'
+    not_perf = '_NOT_PERFORMED'
+    if policy.endswith(not_perf):
+        policy_name = policy[:-len(not_perf)].replace('_',' ')
+        policy_action = 'Not Performed'
+    else:
+        policy_name = policy[:-len(perf)].replace('_',' ')
+        policy_action = 'Performed'
+
+    return policy_name, policy_action
+
 model_files = {
         # format
         # model : [vectorizer, neural net]
-        'base' : ['count_vec_for_nn.pckl', 
+        'base' : ['count_vec_for_nn.pckl',
                   'nn_model_first_trial.h5'],
-        
-        'reduced_feats' : ['vectorizer_uni_bi_min_5_max_10k.pckl', 
+
+        'reduced_feats' : ['vectorizer_uni_bi_min_5_max_10k.pckl',
                            'nn_model_uni_bi_min_5_max_10k.h5'],
-        
-        'stop_words' : ['vectorizer_uni_bi_min_5_max_10k_stop.pckl', 
+
+        'stop_words' : ['vectorizer_uni_bi_min_5_max_10k_stop.pckl',
                         'nn_model_uni_bi_min_5_max_10k_stop.h5'],
-        
-        'tfidf' : ['vectorizer_tfidf_uni_bi_min_5_max_10k.pckl', 
+
+        'tfidf' : ['vectorizer_tfidf_uni_bi_min_5_max_10k.pckl',
                    'nn_model_tfidf_uni_bi_min_5_max_10k.h5']
         }
 
-def main(url='https://www.snap.com/en-US/privacy/privacy-policy', model_name='reduced_feats'):
-#def main(url='http://www.seriously.com/privacy-notice/', model_name='tfidf'):
-    
-    # start 
-    print('-------------------------------')
-    print('Evaluating '+ model_name.upper())
-    
+#def main(url='https://www.snap.com/en-US/privacy/privacy-policy', model_name='base'):
+def main(url='http://www.seriously.com/privacy-notice/', model_name='reduced_feats'):
+
+    # refresh session
+    K.clear_session()
+
+    # start
+    #print('-------------------------------')
+    #print('Evaluating '+ model_name.upper())
+
     # get html stuff
     full_policy = get_html_from_url(url)
     all_sentences = get_sentences_from_html(full_policy)
 
     # load models
-    pickle_jar = '../pickles/nn_models/'
+    base_path = '/Users/Khaldoon/Dropbox/insight/inphormed/'
+    #base_path = 'inphormed/static/'
+    pickle_path = 'pickles/nn_models/'
+    pickle_jar = base_path + pickle_path
     vectorizer = pickle.load(open(os.path.join(pickle_jar,model_files[model_name][0]), 'rb'))
     nn_model = load_model(os.path.join(pickle_jar,model_files[model_name][1]))
-    vectorizer = pickle.load(open(os.path.join(pickle_jar,'count_vec_for_nn.pckl'), 'rb'))
-    nn_model = load_model(os.path.join(pickle_jar,'nn_model_first_trial.h5'))
     policy_indices = pickle.load(open(os.path.join(pickle_jar, 'policy_index_dict.pckl'),'rb'))
-    #print('models loaded successfully')
-    
+
     # transform & classify html
     df_oos = pd.DataFrame(all_sentences,columns=['text'])
     df_oos = standardize_text(df_oos, 'text')
@@ -106,23 +129,25 @@ def main(url='https://www.snap.com/en-US/privacy/privacy-policy', model_name='re
     y_oos_pred_numeric = (y_oos_pred>0.5)*1
     #practiced_policies = sum(y_oos_pred>0.5)
     practiced_policies = sum(y_oos_pred_numeric)
-    
-    #print('There are {} sentences'.format(len(all_sentences)))
-    pol_to_sent = {}
+
+    result = {}
+    sentences = {}
+
     for pol in policy_indices:
         if practiced_policies[policy_indices[pol]] > 0:
-            #print(pol)
-            pol_to_sent[pol] = []
+            pol_name, pol_action = get_policy_action(pol)
+            result[pol_name] = pol_action
+            sentences[pol_name] = []
             ind = 0
             for res_array in y_oos_pred_numeric:
                 if res_array[policy_indices[pol]] > 0:
-                    #print('match at sentence {}'.format(ind))
-                    #print(all_sentences[ind])
-                    pol_to_sent[pol].append(all_sentences[ind])
+                    sentences[pol_name].append(all_sentences[ind])
                 ind +=1
-    print(pol_to_sent)
-    print(pol_to_sent.keys())
 
+    #print(result)
+    #print(sentences)
+    #return result
+    return result, sentences
 
 if __name__ == '__main__':
     main()
--- flaskapp/inphormed/home.py
+++ flaskapp/inphormed/home.py
@@ -1,7 +1,15 @@
 from flask import request, render_template
 from inphormed import app
 #from inphormed import policy_pull
-from inphormed import evaluate_policy
+#from inphormed import evaluate_policy
+import pickle
+from inphormed import evaluate_policy_dynamic
+
+#list_file = 'policy_list.pckl'
+#list_file = 'policy_list_ord_all.pckl'
+list_file = 'policy_list_ord_top.pckl'
+policy_tmp = pickle.load(open('inphormed/static/pickles/'+list_file,'rb'))
+policy_list = [p.replace('_',' ') for p in policy_tmp]
 
 @app.route('/')
 def index():
@@ -16,14 +24,36 @@ def about():
 @app.route('/', methods=['POST'])
 def result():
     url = request.form['url']
-    tmp_result = evaluate_policy.main(url=url)
-    icons = {'Performed':'&#10060;','Not Performed':'&#9989;'}
+    # ADD THIS AFTER MORE INPUT CAN BE HANDLED
+    #practices = request.form['practices']
+    #chosen_policies = []
+    #for a in practices:
+    #   for b in policy_list:
+    #       if b.casefold().contains(a.casefold())
+    #           chosen_policies.append(b)
+    #tmp_result = evaluate_policy.main(url=url)
+    tmp_result, tmp_sentences = evaluate_policy_dynamic.main(url=url)
+    icons = {'Performed':'&#10060;','Not Performed':'&#9989;','Not Mentioned':'&#128679;'}
     result = {}
-    for pol in tmp_result:
-        tmp_dict = {}
-        tmp_dict['modality'] = tmp_result[pol]
-        tmp_dict['icon'] = icons[tmp_result[pol]]
-        result[pol] = tmp_dict
-        
+
+    # ADD:
+    # option to take input for TYPES of policies user is interested if __name__ == '__main__':
+    # show results for policies of interest (Location, Contacts, Media, etc)
+
+    #for pol in chosen_policies:
+    for pol in policy_list:
+        if pol in tmp_result:
+            tmp_dict = {}
+            tmp_dict['modality'] = tmp_result[pol]
+            tmp_dict['sentences'] = tmp_sentences[pol]
+            tmp_dict['icon'] = icons[tmp_result[pol]]
+            result[pol] = tmp_dict
+        else:
+            tmp_dict = {}
+            tmp_dict['modality'] = 'Not Mentioned'
+            tmp_dict['sentences'] = ['This practice is not explicitly mentioned in the policy']
+            tmp_dict['icon'] = icons['Not Mentioned']
+            result[pol] = tmp_dict
+
     return render_template('result.html',
                             policy=result)
